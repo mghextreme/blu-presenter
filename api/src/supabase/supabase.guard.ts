e44add232408456a53bea5e1ac2396fc7d45c4c8
Matias Henschel <matias@fleetrover.com>
2024-07-07 21:59:09 -0300

    Fix public routes after auth passport change.

--- api/src/supabase/supabase.guard.ts
+++ api/src/supabase/supabase.guard.ts
@@ -1,5 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
-export class SupabaseGuard extends AuthGuard('jwt') {}
+export class SupabaseGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const result = (await super.canActivate(context)) as boolean;
+    return result;
+  }
+}
