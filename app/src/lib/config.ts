29f2c61e7e910a95c610a155fe9706b19d025246
Matias Henschel <matias@fleetrover.com>
2025-05-25 14:53:45 -0300

    Add build and deploy actions

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -5,19 +5,13 @@ interface IMetaEnv {
 
 const metaEnv = (import.meta as unknown as IMetaEnv).env
 
+const protocol = metaEnv.VITE_API_PROTOCOL || 'http';
 const host = metaEnv.VITE_API_HOST;
-const port = parseInt(metaEnv.VITE_API_PORT || '3000', 10);
+const port = parseInt(metaEnv.VITE_API_PORT || '0', 10);
+const url = `${protocol}://${host}` + (port > 0 ? `:{port}` : '');
 
 export const api = {
   host,
   port,
-  url: `http://${host}:${port}`,
-}
-
-const supabaseUrl = metaEnv.VITE_SUPABASE_URL;
-const supabaseKey = metaEnv.VITE_SUPABASE_KEY;
-
-export const supabase = {
-  url: supabaseUrl,
-  key: supabaseKey,
+  url,
 }
