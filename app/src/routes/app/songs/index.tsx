1d83702a102a9452c4021d469ee265128e3960ab
Matias G Henschel <matiashenschel@hotmail.com>
2026-08-19 22:11:31 -0300

    Re-add buttons to add songs.

--- app/src/routes/app/songs/index.tsx
+++ app/src/routes/app/songs/index.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { ISongWithRole } from "@/types";
 import { useServices } from "@/hooks/useServices";
 import { SearchProvider } from "@/hooks/search.provider";
 import { AdvancedSearchForm } from "@/components/app/search/advanced-search-form";
 import { SongSearchResultsList } from "@/components/app/search/song-search-results-list";
+import { OrganizationBar } from "@/components/app/organization-bar";
+import { Button } from "@/components/ui/button";
 
 export function Songs() {
 
@@ -16,15 +18,21 @@ export function Songs() {
   const data = useLoaderData() as ISongWithRole[];
 
   return (
-    <div className="p-2 sm:p-8">
+    <>
       <title>{t('title.list') + ' - BluPresenter'}</title>
-      <h1 className="text-3xl mb-4">{t('title.list')}</h1>
-      <SearchProvider songsService={songsService} defaultValue={data}>
-        <AdvancedSearchForm />
-        <ul className="mt-4 space-y-2">
-          <SongSearchResultsList />
-        </ul>
-      </SearchProvider>
-    </div>
+      <OrganizationBar organizations={[]}>
+        <Button asChild><Link to="/app/songs/add">{t('actions.create')}</Link></Button>
+        <Button asChild><Link to="/app/songs/import">{t('actions.import')}</Link></Button>
+      </OrganizationBar>
+      <div className="p-2 sm:p-8">
+        <h1 className="text-3xl mb-4">{t('title.list')}</h1>
+        <SearchProvider songsService={songsService} defaultValue={data}>
+          <AdvancedSearchForm />
+          <ul className="mt-4 space-y-2">
+            <SongSearchResultsList />
+          </ul>
+        </SearchProvider>
+      </div>
+    </>
   );
 }
